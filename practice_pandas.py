a30885632cbda69d3b65c86a07d665288204b293
saitonao <saito.nao@mic-p.com>
2024-06-03 13:38:15 +0900

    third commit

--- practice_pandas.py
+++ practice_pandas.py
@@ -1,23 +1,42 @@
 import pandas as pd
 
-#series_data = [10,15,20,25]
-#ser = pd.Series(series_data)
 
-#print(ser)
+df = pd.read_csv("./titanic.csv")
 
-dataframe_data = ([
-    [100, "a", True],
-    [200, "b", False],
-    [150, "d", False],
-    [550, "c", True]
-])
-df = pd.DataFrame(dataframe_data)
-df.index=["a","b","c","d"]
+# print(df)
+# print(df[["Age","Name"]])
 
-df.columns=['A','B','C']
-print(df)
+#print(df.tail(3))
 
-print(df[['A','B']])
+#print(df.iloc[2:5,0:5])
+
+# print(df.loc[[2,5]]) # 行の指定
+# print(df.loc[:,["Age","Name"]]) # 列の指定
+# print(df.loc[[2,5],["Age","Name"]]) # 行、列の指定
+
+# print(df.at[2,"Name"])
+
+# result = df["Age"].isin([20,30,40])
+
+# print(df[result])
+
+# sorted_df = df.sort_values("PassengerId",ascending=False)
+# print(sorted_df)
+
+
+#print(df.dropna(how="any"))
+#print(df.fillna(value=0))
+# print(df.isna())
+
+
+# print(df.groupby(["Gender","Survived"])["Survived"].count())
+
+# print(df.groupby(["Gender"])["Age"].std())
+
+# for column in df:
+#     print(column)
 
 
 
+for age, pclass in zip(df["Age"],["Pclass"]):
+    print(age,pclass)
