659e2a2c7fad135404d6e5fbfc13bddf81501c8b
saitonao <saito.nao@mic-p.com>
2024-06-05 17:58:09 +0900

    commit

--- final.py
+++ final.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pyodbc
+import openpyxl
 from openpyxl import Workbook, load_workbook
 import os
 from dotenv import load_dotenv
@@ -20,7 +21,11 @@ conn.commit()
 
 
 #テーブル作成
-cursor.execute("CREATE TABLE newmovies(id int IDENTITY(1,1), Release_Date date, Title varchar(1000), Overview varchar(2000),Popularity varchar(1000),Vote_Count int ,Vote_Average int ,Original_Language  varchar(1000) ,Genre varchar(1000) ,Poster_Url varchar(1000))")
+cursor.execute(
+    '''
+    CREATE TABLE newmovies(id int IDENTITY(1,1), Release_Date date, Title varchar(1000), Overview varchar(2000),Popularity varchar(1000),Vote_Count int ,Vote_Average int ,Original_Language  varchar(1000) ,Genre varchar(1000) ,Poster_Url varchar(1000))")
+    '''
+)
 conn.commit()
 
 
@@ -28,48 +33,61 @@ conn.commit()
 #作成したテーブルに CSV データを入れる
 df = pd.read_csv('./mymoviedb.csv',lineterminator='\n')
 for row in df.itertuples():
-    cursor.execute("INSERT INTO newmovies(Release_Date, Title, Overview,Popularity,Vote_Count,Vote_Average,Original_Language,Genre,Poster_Url) values(?,?,?,?,?,?,?,?,?)",row.Release_Date,row.Title,row.Overview,row.Popularity,row.Vote_Count,row.Vote_Average,row.Original_Language,row.Genre,row.Poster_Url)
-
-
-# conn.commit()
+    cursor.execute(
+        '''
+        INSERT INTO newmovies(Release_Date, Title, Overview,Popularity,Vote_Count,Vote_Average,Original_Language,Genre,Poster_Url) 
+        values(?,?,?,?,?,?,?,?,?)
+        ''',
+        row.Release_Date,
+        row.Title,
+        row.Overview,
+        row.Popularity,
+        row.Vote_Count,
+        row.Vote_Average,
+        row.Original_Language,
+        row.Genre,
+        row.Poster_Url
+)
+        
+conn.commit()
 
 #データベースからデータ取得
-df = pd.read_sql('SELECT * FROM newmovies',conn)
+df_all = pd.read_sql('SELECT * FROM newmovies',conn)
 
 
 #言語ごとの映画の数を出力する
-print(df.groupby(["Original_Language"])["Original_Language"].count())
+df_lan = df_all.groupby(["Original_Language"])["Original_Language"].count()
 
 #ジャンルごとに平均値を出す
-print(df.groupby(["Genre"])["Vote_Average"].mean())
+df_gen = df_all.groupby(["Genre"])["Vote_Average"].mean()
 
 #分析結果を Excel で保存
-import openpyxl
+
 wb = Workbook()
 ws_1 = wb.active
 ws_1.title = "Language Counts"
 ws_2 = wb.create_sheet(title="Genre Ratings")
 
 #Original Language
-data1 = df.groupby(["Original_Language"])["Original_Language"].count()
+df_lan = df.groupby(["Original_Language"])["Original_Language"].count()
 
 ws_1.cell(1,1,"Original Language")
 ws_1.cell(1,2,"Count")
 
 count = 1
-for country, number in data1.items():
+for country, number in df_lan.items():
     ws_1.cell(count +1, 1,country)
     ws_1.cell(count +1, 2,number)
     count +=1
 
 
 #Genre Ratings
-data2 = df.groupby(["Genre"])["Vote_Average"].mean()
+df_gen = df.groupby(["Genre"])["Vote_Average"].mean()
 ws_2.cell(1,1,"Genre")
 ws_2.cell(1,2,"Vote_Average")
 
 count=1
-for genre, average in data2.items():
+for genre, average in df_gen.items():
     ws_2.cell(count +1,1,genre)
     ws_2.cell(count +1,2,average)
     count +=1
--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-from my_module import add
+list = [5,1,4,6,2]
+
+# for i in range(len(list)):
+#     for j in range(len(list)-1):
+#         if list[j] > list[j + 1]:
+#             list[j], list[j + 1] = list[j + 1],list[j]
+            
+# for i in range(len(list)):
+#     for j in range(len(list) - 1):
+#         if list[j] > list[j + 1]:
+#             temp = list[j]
+#             list[j]=list[j+1]
+#             list[j+1]= temp
+
+list = [5,1,4,6,2]
+
+search = 8
+search_index = None
+for index in range(len(list)):
+    data = list[index]
+    if data == search:
+        search_index = index
+        break
+
+if search_index == None:
+    print("Not Found")
+else:
+    print(search_index)
+
+
+
+
+# line = "{name}さんは{age}歳で、{prefecture}出身です。".format(name="佐川",age=20,prefecture="北海道")
+# print(line)
+
+
+# data = "080-5638-4169"
+# data = data.replace("-","")
+# print(data)
